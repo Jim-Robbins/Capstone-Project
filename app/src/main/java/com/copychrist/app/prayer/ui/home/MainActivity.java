bdea57d99e52e1c6a912b2eb59a9ffc7c4348e29
Jim Robbins <jrobns@gmail.com>
2017-09-19 23:50:10 -0400

    Restore home page. Update login and home layouts.

---
+++ app/src/main/java/com/copychrist/app/prayer/ui/home/MainActivity.java
@@ -0,0 +1,94 @@
+package com.copychrist.app.prayer.ui.home;
+
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.copychrist.app.prayer.R;
+import com.copychrist.app.prayer.ui.BaseActivity;
+import com.copychrist.app.prayer.ui.contactgroups.ContactGroupActivity;
+import com.copychrist.app.prayer.ui.login.LoginActivity;
+import com.copychrist.app.prayer.ui.prayerlist.PrayerListActivity;
+import com.copychrist.app.prayer.ui.prayerrequest.PrayerRequestDetailActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity implements MainContract.View {
+
+    @BindView(R.id.icon_container_prayer_lists) RelativeLayout iconPrayerLists;
+    @BindView(R.id.icon_container_contact_groups) RelativeLayout iconContactGroups;
+    @BindView(R.id.icon_container_prayer_requests) RelativeLayout iconPrayerRequests;
+    @BindView(R.id.icon_container_contacts) RelativeLayout iconContacts;
+    @BindView(R.id.icon_container_bible_verses) RelativeLayout iconBibleVerses;
+    @BindView(R.id.icon_container_logout) RelativeLayout iconLogout;
+
+    @Inject MainContract.Presenter mainPresenter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected Object getModule() {
+        return new MainModule();
+    }
+
+    @Override
+    public void showLogin() {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainPresenter.setView(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainPresenter.clearView();
+    }
+
+    @OnClick(R.id.icon_container_prayer_lists)
+    public void onPrayerListsClick() {
+        startActivity(PrayerListActivity.getStartIntent(this, null));
+    }
+
+    @OnClick(R.id.icon_container_prayer_requests)
+    public void onPrayerRequestsClick() {
+        startActivity(PrayerRequestDetailActivity.getStartAddIntent(this, "zzz"));
+    }
+
+    @OnClick(R.id.icon_container_contact_groups)
+    public void onContactGroupsClick() {
+        startActivity(ContactGroupActivity.getStartIntent(this, null));
+    }
+
+    @OnClick(R.id.icon_container_contacts)
+    public void onContactsClick() {
+        //TODO :: hook up a simple presenter to save verses
+//        BiblePassageFinderDialogFragment biblePassageFinderDialogFragment = BiblePassageFinderDialogFragment.newInstance();
+//        biblePassageFinderDialogFragment.show(getSupportFragmentManager(), "BiblePassageFinderDialogFragment");
+    }
+
+    @OnClick(R.id.icon_container_bible_verses)
+    public void onBibleVersesClick() {
+        //startActivity(BibleVerseActivity.getStartIntent(this, null));
+    }
+
+    @OnClick(R.id.icon_container_logout)
+    public void setIconLogoutClick() {
+        FirebaseDatabase.getInstance().goOffline();
+        FirebaseAuth.getInstance().signOut();
+        startActivity(LoginActivity.getStartIntent(this));
+    }
+}
---
+++ app/src/main/java/com/copychrist/app/prayer/ui/home/MainService.java
@@ -0,0 +1,15 @@
+package com.copychrist.app.prayer.ui.home;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by jim on 9/11/17.
+ */
+
+public class MainService {
+
+    MainService(FirebaseDatabase database, FirebaseUser user) {
+        
+    }
+}
